2d76ad1e8d56a1482c8d23438bf9fac8faba82b7
lena sano <magdalena.sano@splitsoftware.com>
2024-05-13 19:39:21 -0300

    Initial Commit

---
+++ Coffee Tracker iOS/Coffee_Tracker_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_Tracker_iOSApp.swift
+//  Coffee Tracker iOS
+//
+//  Created by lena on 13/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_Tracker_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
