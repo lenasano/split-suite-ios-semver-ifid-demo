f9b728700ef5920caf16cb6068557ff49b0d58b5
lena sano <magdalena.sano@splitsoftware.com>
2024-07-01 23:44:52 -0300

    set up error

--- Coffee Tracker iOS/CoffeeTrackeriOSApp.swift
+++ Coffee Tracker iOS/CoffeeTrackeriOSApp.swift
@@ -11,35 +11,35 @@ import os
 
 @main
 struct CoffeeTrackeriOSApp: App {
-    /*@StateObject*/ private var split = SplitWrapper.instance
+    @StateObject private var split = SplitWrapper.instance
     
     let logger = Logger(subsystem: "splitio-examples.Coffee-Tracker-iOS.CoffeeTrackeriOSApp", category: "Root View")
     
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                /*let isAsync = split.evaluateFeatureFlagUsingAttributes(SplitWrapper.flag.isAsyncOn)!
+                let isAsync = split.evaluateFeatureFlagUsingAttributes(SplitWrapper.flag.isAsyncOn)!
                 
-                if( isAsync.starts(with: "on") ) {*/
+                if( isAsync.starts(with: "on") ) {
                     ContentViewAsync()
-                /*} else {
+                } else {
                     ContentView()
-                }*/
+                }
             }
             .onAppear() {
                 logger.debug("App appeared!")
             }
-            //.disabled(!split.isReady && !split.isReadyTimedOut)
-            //.overlay(loadingOverlay)
+            .disabled(!split.isReady && !split.isReadyTimedOut)
+            .overlay(loadingOverlay)
             .environment(\.colorScheme, .dark)
             //.environmentObject(split) - we can pass the SplitWrapper to descendant views, but it's not needed in this demo
         }
     }
     
     @ViewBuilder private var loadingOverlay: some View {
-        //if !split.isReady && !split.isReadyTimedOut {
+        if !split.isReady && !split.isReadyTimedOut {
             ProgressView()
-        //}
+        }
     }
 }
 
